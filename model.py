060b1e87b4ec6b49ee19fd8a25a9512264ac261e
nilsquiring <nilsquiring@gmail.com>
2018-11-21 00:27:35 +0100

    Update model.py

--- model.py
+++ model.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LinearRegression
+from sklearn.naive_bayes import MultinomialNB
 
 email_df = pd.read_pickle('email_cleaned')
 
@@ -22,7 +24,6 @@ def VectCount(X_train, X_test):
     test_matrix_count = vectorizer_count.transform(X_test)
     return train_matrix_count, test_matrix_count
 
-
 def VectTfidf(X_train, X_test):
     vectorizer_tfidf = TfidfVectorizer(token_pattern = r'\b\w+\b')
     train_matrix_tfidf = vectorizer_tfidf.fit(X_train)
@@ -31,3 +32,16 @@ def VectTfidf(X_train, X_test):
 
 
 train_matrix_count, test_matrix_count = VectCount(X_train, X_test)
+
+model_bayes = MultinomialNB()
+model_bayes.fit(train_matrix_count, y_train)
+model_bayes.predict(test_matrix_count)
+model_bayes.score(test_matrix_count, y_test)
+
+
+"""
+model_linear = LinearRegression()
+model_linear.fit(train_matrix_count, y_train)
+model_linear.predict(test_matrix_count)
+model_linear.score(test_matrix_count, y_test)
+"""
