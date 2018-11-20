d36f0f9a51a399c338d86768cce045d31d50c17b
undercover95 <mbularz95@interia.pl>
2018-11-20 12:54:17 +0100

    Adding comment

--- Spam_detection.py
+++ Spam_detection.py
@@ -15,9 +15,11 @@ import string
 import unicodedata
 import sys
 
-tbl = dict.fromkeys(i for i in range(sys.maxunicode) if unicodedata.category(chr(i)).startswith('P'))
+tbl = dict.fromkeys(i for i in range(sys.maxunicode)
+                    if unicodedata.category(chr(i)).startswith('P'))
 
-psutil.virtual_memory().percent #returns the percentile of used ram, using as a break to not shutdown everything
+# returns the percentile of used ram, using as a break to not shutdown everything
+psutil.virtual_memory().percent
 
 path1 = 'Data/*gz'
 files = glob.glob(path1)
@@ -39,8 +41,8 @@ for name in files[2:]:
             f = tar.extractfile(member)
             if f is not None:
                 spam.append(f.read())
- 
-               
+
+
 subject = b"\n"
 spam_subjects = []
 
@@ -48,17 +50,19 @@ to_del = []
 
 for i in tqdm.tqdm(range(len(spam))):
     index = spam[i].find(subject)
-    spam_subjects.append(spam[i][9:index]) #from index 9 to get rid of 'Subject: '
+    # from index 9 to get rid of 'Subject: '
+    spam_subjects.append(spam[i][9:index])
     text = spam[i][index + len(subject):]
     if text != b'':
         try:
-            spam[i] = set(text.decode("utf-8").replace("\n"," ").replace("\r"," ").translate(tbl).lower().split(' '))
+            spam[i] = set(text.decode("utf-8").replace("\n",
+                                                       " ").replace("\r", " ").translate(tbl).lower())
         except UnicodeDecodeError:
             to_del.append(i)
     else:
         spam[i] = ''
 
-for item in reversed(to_del): #removing all emails that couldnt be decoded
+for item in reversed(to_del):  # removing all emails that couldnt be decoded
     del spam[item]
     del spam_subjects[item]
 
@@ -67,28 +71,30 @@ to_del = []
 ham_subjects = []
 for i in tqdm.tqdm(range(len(ham))):
     index = ham[i].find(subject)
-    ham_subjects.append(ham[i][9:index]) #from index 9 to get rid of 'Subject: '
+    # from index 9 to get rid of 'Subject: '
+    ham_subjects.append(ham[i][9:index])
     text = ham[i][index + len(subject):]
     if text != b'':
         try:
-            ham[i] = set(text.decode("utf-8").replace("\n"," ").replace("\r"," ").translate(tbl).lower().split(' '))
-        except UnicodeDecodeError:  
+            ham[i] = set(text.decode("utf-8").replace("\n",
+                                                      " ").replace("\r", " ").translate(tbl).lower().split(' '))
+        except UnicodeDecodeError:
             to_del.append(i)
     else:
         ham[i] = ''
 
-for item in reversed(to_del): #removing all emails that couldnt be decoded
+for item in reversed(to_del):  # removing all emails that couldnt be decoded
     del spam[item]
     del spam_subjects[item]
 
 
+# end of preprocessing data
+print(ham[0])
 
-#np.save('spam.npy',spam)
-#np.save('spam_sub.npy',spam_subjects)
-#np.save('ham.npy',ham)
-#np.save('ham_sub.npy',ham_subjects)
+# np.save('spam.npy',spam)
+# np.save('spam_sub.npy',spam_subjects)
+# np.save('ham.npy',ham)
+# np.save('ham_sub.npy',ham_subjects)
 
-#df.to_pickle(file_name)
-#pd.read_pickle(file_name)
-
-
+# df.to_pickle(file_name)
+# pd.read_pickle(file_name)
