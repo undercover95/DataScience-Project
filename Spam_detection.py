efee05d2f4edaac12715e2e8d37c4db4615ac99d
undercover95 <mbularz95@interia.pl>
2018-11-20 13:25:41 +0100

    Preprocess email data

--- Spam_detection.py
+++ Spam_detection.py
@@ -13,6 +13,10 @@ import tqdm
 import string
 import unicodedata
 import sys
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 
 tbl = dict.fromkeys(i for i in range(sys.maxunicode)
                     if unicodedata.category(chr(i)).startswith('P'))
@@ -26,7 +30,7 @@ data = []
 ham = []
 spam = []
 
-for name in files[2:]:
+for name in files:
     tar = tarfile.open(name, "r:gz")
     for member in tar.getmembers():
         if('ham' in member.name):
@@ -86,12 +90,25 @@ for item in reversed(to_del):  # removing all emails that couldnt be decoded
 
 
 # end of preprocessing data
-print(ham[0])
+spam_data = {
+    'clean_msg': spam,
+    'target': np.ones(len(spam))*(-1)
+}
+spam_data_df = pd.DataFrame(data=spam_data)
+
+ham_data = {
+    'clean_msg': ham,
+    'target': np.ones(len(ham))
+}
+ham_data_df = pd.DataFrame(data=ham_data)
+
+data_df = pd.concat([spam_data_df, ham_data_df])
+data_df = data_df.reset_index(drop=True)
 
 # np.save('spam.npy',spam)
 # np.save('spam_sub.npy',spam_subjects)
 # np.save('ham.npy',ham)
 # np.save('ham_sub.npy',ham_subjects)
 
-# df.to_pickle(file_name)
+data_df.to_pickle('email_cleaned')
 # pd.read_pickle(file_name)
