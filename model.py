c7315e91948f2bcf1a58e47123cacfa357bca780
nilsquiring <nilsquiring@gmail.com>
2018-11-21 17:20:47 +0100

    Update model.py

--- model.py
+++ model.py
@@ -22,20 +22,22 @@ def VectCount(X_train, X_test):
     vectorizer_count = CountVectorizer(token_pattern = r'\b\w+\b')
     train_matrix_count = vectorizer_count.fit_transform(X_train)
     test_matrix_count = vectorizer_count.transform(X_test)
-    return train_matrix_count, test_matrix_count
+    features = vectorizer_count.get_feature_names()
+    return train_matrix_count, test_matrix_count, features
 
 def VectTfidf(X_train, X_test):
     vectorizer_tfidf = TfidfVectorizer(token_pattern = r'\b\w+\b')
     train_matrix_tfidf = vectorizer_tfidf.fit_transform(X_train)
     test_matrix_tfidf = vectorizer_tfidf.transform(X_test)
-    return train_matrix_tfidf, test_matrix_tfidf
+    features = vectorizer_tfidf.get_feature_names()
+    return train_matrix_tfidf, test_matrix_tfidf, features
 
 
-train_matrix_count, test_matrix_count = VectCount(X_train, X_test)
-train_matrix_Tfidf, test_matrix_Tfidf = VectTfidf(X_train, X_test)
+train_matrix_count, test_matrix_count, features_count = VectCount(X_train, X_test)
+train_matrix_Tfidf, test_matrix_Tfidf, features_Tfidf = VectTfidf(X_train, X_test)
 
 
-def model(model_name, train_matrix, test_matrix):
+def model(model_name, train_matrix, test_matrix, features):
     if model_name=='Bayes':
         model = MultinomialNB()
     elif model_name=='Linear':
@@ -44,16 +46,18 @@ def model(model_name, train_matrix, test_matrix):
         return None
     model.fit(train_matrix, y_train)
     print('Test score: {}'.format(model.score(test_matrix, y_test)))
-    return model
+    coefs_sorted, features_sorted = zip(*sorted(zip(model.coef_[0],features)))
+    #model.predict_proba(test_matrix)
+    return model, coefs_sorted, features_sorted
 
 print('Lets start the fitting!')
 
-model_bayes_count = model('Bayes',train_matrix_count, test_matrix_count)
+model_bayes_count, coefs_sorted_count, features_sorted_count = model('Bayes',train_matrix_count, test_matrix_count, features_count)
 
 print('Model number 2')
 
-model_bayes_Tfidf = model('Bayes',train_matrix_Tfidf, test_matrix_Tfidf)
+model_bayes_Tfidf, coefs_sorted_Tfidf, features_sorted_Tfidf = model('Bayes',train_matrix_Tfidf, test_matrix_Tfidf, features_Tfidf)
 
-#model_linear_count = model('Linear',train_matrix_count, test_matrix_count)
+#model_linear_count = model('Linear',train_matrix_count, test_matrix_count, features_count)
 
-#model_linear_Tfidf = model('Linear',train_matrix_Tfidf, test_matrix_Tfidf)
+#model_linear_Tfidf = model('Linear',train_matrix_Tfidf, test_matrix_Tfidf, features_Tfidf)
